bbfa0f3dae2bb39858437c4c578121f788bae927
Mohamed A.Abdelalim <51891522+abdelalim2000@users.noreply.github.com>
2023-07-10 19:10:04 +0300

    Update filament-dynamic-settings-page.php

--- config/filament-dynamic-settings-page.php
+++ config/filament-dynamic-settings-page.php
@@ -20,5 +20,13 @@ return [
         //  permission name 
         // 'name' => 'list.settings'
         'name' => ''
+    ],
+    // assets change for banned ip addreses
+    'assets' => [
+        'select2-min-css' => 'https://cdnjs.cloudflare.com/ajax/libs/select2/4.1.0-beta.0/css/select2.min.css',
+        'select2-min-js' => 'https://cdnjs.cloudflare.com/ajax/libs/select2/4.1.0-beta.0/js/select2.min.js',
+        'jquery' => 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js',
+        'jquery-ui-js' => 'https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.13.2/jquery-ui.min.js',
+        'jquery-ui-css' => 'https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.13.2/themes/base/jquery-ui.min.css',
     ]
 ];
